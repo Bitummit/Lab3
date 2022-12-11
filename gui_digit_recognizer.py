4c17b46c3c3b5f907384ad43f6fd327d1408c1f2
Bitummit <ignat.700@mail.ru>
2022-12-11 16:30:30 +0300

    123

---
+++ gui_digit_recognizer.py
@@ -0,0 +1,69 @@
+import os
+
+import win32gui
+os.environ['TF_CPP_MIN_LOG_LEBEL'] = '2'
+from keras.models import load_model
+from tkinter import *
+import tkinter as tk
+from PIL import ImageGrab, Image, ImageOps
+import numpy as np
+import matplotlib.pyplot as plt
+
+model = load_model('16_model.h5')
+
+
+def predict_digit(img):
+    img = img.resize((28,28))
+    img = ImageOps.invert(img)
+    img = img.convert('L')
+    img = np.array(img)
+
+    img1 = img.reshape(-1, 28, 28, 1)
+    img1 = img1 / 255
+
+    res = model.predict(img1)
+    print(res)
+    res = list(res)
+    return np.argmax(res)
+
+
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.x = self.y = 0
+
+        # Создание элементов
+        self.canvas = tk.Canvas(self, width=300, height=300, bg="white", cursor="cross")
+        self.label = tk.Label(self, text="Думаю..", font=("Helvetica", 48))
+        self.classify_btn = tk.Button(self, text="Распознать", command=self.classify_handwriting)
+        self.button_clear = tk.Button(self, text="Очистить", command=self.clear_all)
+
+        # Сетка окна
+        self.canvas.grid(row=0, column=0, pady=2, sticky=W, )
+        self.label.grid(row=0, column=1, pady=2, padx=2)
+        self.classify_btn.grid(row=1, column=1, pady=2, padx=2)
+        self.button_clear.grid(row=1, column=0, pady=2)
+
+        self.canvas.bind("<B1-Motion>", self.draw_lines)
+
+    def clear_all(self):
+        self.canvas.delete("all")
+
+    def classify_handwriting(self):
+        HWND = self.canvas.winfo_id()
+        rect = win32gui.GetWindowRect(HWND)  # получаем координату холста
+        im = ImageGrab.grab(rect)
+
+        digit = predict_digit(im)
+        self.label.configure(text=str(digit))
+
+    def draw_lines(self, event):
+        self.x = event.x
+        self.y = event.y
+        r = 8
+        self.canvas.create_oval(self.x - r, self.y - r, self.x + r, self.y + r, fill='black')
+
+
+app = App()
+mainloop()
---
+++ main.py
@@ -0,0 +1,26 @@
+import keras
+from keras.datasets import mnist
+from keras.layers import Dense, Flatten
+
+if __name__ == '__main__':
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train / 255
+    x_test = x_test / 255
+
+    y_train_cat = keras.utils.to_categorical(y_train, 10)
+    y_test_cat = keras.utils.to_categorical(y_test, 10)
+
+    model = keras.Sequential([
+        Flatten(input_shape=(28, 28, 1)),
+        Dense(128, activation='relu'),
+        Dense(10, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train_cat, batch_size=20, epochs=10, validation_split=0.2)
+
+    model.save('16_model.h5')
